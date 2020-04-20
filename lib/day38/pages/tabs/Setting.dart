e47bdf5c9e8eaa47d179a585744f14bf25660448
isAlbertYu <iameinstein24@qq.com>
2020-04-20 15:42:54 +0800

    学习了TextField、Checkbox，以及其值的获取

---
+++ lib/day38/pages/tabs/Setting.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class SettingPage extends StatefulWidget {
+  @override
+  _SettingPageState createState() => _SettingPageState();
+}
+
+class _SettingPageState extends State<SettingPage> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[
+        ListTile(
+          title: Text("我是一个文本"),
+        ),
+        ListTile(
+          title: Text("我是一个文本"),
+        ),
+        ListTile(
+          title: Text("我是一个文本"),
+        ),
+        ListTile(
+          title: Text("我是一个文本"),
+        ),
+      ],
+    );
+  }
+}
