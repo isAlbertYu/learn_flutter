26584c68391b265f554f5691c8295fb931ab64a3
isAlbertYu <iameinstein24@qq.com>
2020-04-21 18:19:53 +0800

    实现了类似闲鱼的底部导航凸起按钮

---
+++ lib/day37/pages/Tabs.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/material.dart';
+import 'tabs/Home.dart' show HomePage;
+import 'tabs/Setting.dart' show SettingPage;
+import 'tabs/Category.dart' show CategoryPage;
+
+//Tabs: App的首页页面布局
+//含有appbar和底部导航栏
+//凡是页面布局，需含Scaffold组件
+class Tabs extends StatefulWidget {
+  @override
+  _TabsState createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex = 0;
+  List _list = [
+    HomePage(),
+    CategoryPage(),
+    SettingPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Flutter demo"),
+      ),
+      body: this._list[this._currentIndex],
+      floatingActionButton: Container(
+        height: 100,
+        width: 100,
+        padding: EdgeInsets.all(10),
+        margin: EdgeInsets.only(top: 10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100/2),
+          color: Colors.white,
+        ),
+        child: FloatingActionButton(
+          child: Icon(Icons.add),
+          backgroundColor: this._currentIndex == 1 ? Colors.red : Colors.yellow,
+          onPressed: () {
+            setState(() {
+              this._currentIndex = 1;
+            });
+          },
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: this._currentIndex,
+        onTap: (int index) {
+          setState(() {
+            this._currentIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("首页"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            title: Text("分类"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            title: Text("设置"),
+          ),
+        ],
+        selectedIconTheme: IconThemeData(
+          color: Colors.green,
+          size: 50,
+        ),
+        unselectedIconTheme: IconThemeData(
+          color: Colors.red,
+          size: 50,
+        ),
+        type: BottomNavigationBarType.fixed,
+      ),
+      drawer: Drawer(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: UserAccountsDrawerHeader(
+                    accountName: Text("哈哈哈"),
+                    accountEmail: Text("123456@qq.com"),
+                    currentAccountPicture: CircleAvatar(
+                      backgroundImage: AssetImage('assets/111.png'),
+                    ),
+                    decoration: BoxDecoration(
+                      image: DecorationImage(
+                        image: NetworkImage(
+                            "https://www.itying.com/images/flutter/2.png"),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    otherAccountsPictures: <Widget>[
+                      Image.network(
+                          "https://www.itying.com/images/flutter/4.png"),
+                      Image.network(
+                          "https://www.itying.com/images/flutter/5.png"),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.home),
+              ),
+              title: Text("我的空间"),
+            ),
+            Divider(thickness: 3),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.people),
+              ),
+              title: Text("用户中心"),
+              onTap: () {
+                print("aaaaass");
+                Navigator.pop(context);
+                Navigator.pushNamed(context, "/user");
+              },
+            ),
+            Divider(thickness: 3),
+            ListTile(
+              leading: CircleAvatar(
+                child: Icon(Icons.settings),
+              ),
+              title: Text("设置中心"),
+            ),
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Text("右边侧边栏"),
+      ),
+    );
+  }
+}
